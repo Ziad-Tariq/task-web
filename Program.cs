58b1dc76c987f5a0658f178d4d1f941aa997d935
Ziad <Ziad@DESKTOP-AIEBT1P>
2022-06-28 15:33:03 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+//add Service for MVC
+builder.Services.AddControllersWithViews(); var app = builder.Build();
+//configure pipline for Dev
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+//Adding Middlewares
+
+app.UseStaticFiles(); app.UseRouting();
+app.MapControllerRoute(
+name: "default",
+pattern: "{Controller=Home}/{action=Index}/{id?}");
+app.Run();
