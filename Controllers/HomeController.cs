fc2edf17a2948b418cff77bac324edfb1774c606
Ziad <Ziad@DESKTOP-AIEBT1P>
2022-07-02 01:14:34 -0700

    quiz 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,9 +4,33 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.Message = "Message is Recieved"
             return View();
+        }
+        public IActionResult task(string fname, string lanme, string email, char pno)
+        {
+
+            return View(Index);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
